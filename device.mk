k20pro9stsv/device_xiaomi_raphael

raphael: copy vendor.lineage.biometrics.fingerprint.inscreen.xml to system/etc/permissions
Signed-off-by: UtsavisGreat <utsavbalar1231@gmail.com>

device.mk
@@ -58,6 +58,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     lineage.biometrics.fingerprint.inscreen@1.0-service.xiaomi_raphael
 
+PRODUCT_COPY_FILES += \
+    vendor/aosip/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:system/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
+
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \

Cosmic-Devices/android_device_xiaomi_raphael

device.mk
@@ -57,6 +57,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     lineage.biometrics.fingerprint.inscreen@1.0-service.xiaomi_raphael
 
+PRODUCT_COPY_FILES += \
+    vendor/aosp/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:system/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
+
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \