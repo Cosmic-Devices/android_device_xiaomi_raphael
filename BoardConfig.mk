1048e747c0a14070b1024b98a021597ecf1f2a9a
Sagar Shah <sagarshah2326@gmail.com>
2019-09-25 16:19:33 +0200

    raphael: Switch to old kernel inclusion name
    Signed-off-by: Sagar Shah <sagarshah2326@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -46,7 +46,7 @@ ifeq ($(TARGET_PREBUILT_KERNEL),)
   TARGET_KERNEL_CONFIG := raphael_defconfig
   TARGET_KERNEL_CLANG_COMPILE := true
   TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
-  TARGET_KERNEL_SOURCE := kernel/xiaomi/raphael
+  TARGET_KERNEL_SOURCE := kernel/xiaomi/sm8150
 endif
 
 # Platform
